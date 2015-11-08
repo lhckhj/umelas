de70394630e8638098d617f24a3099254710f193
lonedune <lhckhj@gmail.com>
2015-11-07 20:40:52 -0500

    common files

---
+++ common/inspinia/js/app.js
@@ -0,0 +1,22 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ */
+(function () {
+    angular.module('inspinia', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',                 // Ui Bootstrap
+        'pascalprecht.translate',       // Angular Translate
+        'ngIdle',                       // Idle timer
+        'ngSanitize',                    // ngSanitize
+    ])
+    .config([  
+        '$locationProvider',
+        function($locationProvider) {
+            $locationProvider.hashPrefix('!');
+        }
+    ])
+})();
+
+// Other libraries are loaded dynamically in the config.js file using the library ocLazyLoad
